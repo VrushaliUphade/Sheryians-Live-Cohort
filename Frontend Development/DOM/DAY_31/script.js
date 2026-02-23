da9b1e044760f5e3239f7b0950a7c50df0e3c053
Vrushali Uphade <vrushaliuphade@gmail.com>
2026-02-23 22:25:18 +0530

    Day-31

---
+++ Frontend Development/DOM/DAY_31/script.js
@@ -0,0 +1,8 @@
+var btn1 = document.querySelector('#btn1');
+var btn2 = document.querySelector('#btn2');
+
+btn1.addEventListener('click',function(){
+   location.href="https://github.com/VrushaliUphade/Sheryians-Live-Cohort/blob/master/Frontend%20Development/DOM/DAY_30/index.html"
+    
+})
+
